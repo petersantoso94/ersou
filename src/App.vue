f8ce3770186a10d316aa429f96379fb8525fb7a1
peter <petersantoso@starlordtech.com>
2020-01-10 15:58:10 +0800

    use vlistitem

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,46 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer app v-model="drawer" v-if="!this.isLandingPage">
-      <v-list dense nav class="py-0">
-        <v-list-item two-line>
-          <v-list-item-avatar>
-            <img src="https://randomuser.me/api/portraits/men/81.jpg" />
-          </v-list-item-avatar>
+	<v-app id="inspire">
+		<v-navigation-drawer app v-model="drawer" v-if="!this.isLandingPage">
+			<v-list dense nav class="py-0">
+				<v-list-item two-line>
+					<v-list-item-avatar>
+						<img src="https://randomuser.me/api/portraits/men/81.jpg" />
+					</v-list-item-avatar>
 
-          <v-list-item-content>
-            <v-list-item-title>Application</v-list-item-title>
-            <v-list-item-subtitle>Subtext</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
+					<v-list-item-content>
+						<v-list-item-title>Application</v-list-item-title>
+						<v-list-item-subtitle>Subtext</v-list-item-subtitle>
+					</v-list-item-content>
+				</v-list-item>
 
-        <v-divider></v-divider>
+				<v-divider></v-divider>
 
-        <v-list-item v-for="item in items" :key="item.title" link>
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
+				<v-list-item v-for="item in items" :key="item.title" :to="item.to" link>
+					<v-list-item-icon>
+						<v-icon>{{ item.icon }}</v-icon>
+					</v-list-item-icon>
 
-          <v-list-item-content>
-            <router-link :to="item.to"
-              ><v-list-item-title>{{
-                item.title
-              }}</v-list-item-title></router-link
-            >
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar app color="primary" dark v-if="!this.isLandingPage">
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <router-link to="/"
-        ><v-img :src="require('@/assets/logo.svg')" max-width="40"></v-img
-      ></router-link>
-      <v-toolbar-title>{{ $route.name }}</v-toolbar-title>
-    </v-app-bar>
-    <v-content>
-      <router-view />
-    </v-content>
-  </v-app>
+					<v-list-item-content>
+						<v-list-item-title>
+							{{
+							item.title
+							}}
+						</v-list-item-title>
+					</v-list-item-content>
+				</v-list-item>
+			</v-list>
+		</v-navigation-drawer>
+		<v-app-bar app color="primary" dark v-if="!this.isLandingPage">
+			<v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+			<router-link to="/">
+				<v-img :src="require('@/assets/logo.svg')" max-width="40"></v-img>
+			</router-link>
+			<v-toolbar-title>{{ $route.name }}</v-toolbar-title>
+		</v-app-bar>
+		<v-content>
+			<router-view />
+		</v-content>
+	</v-app>
 </template>
 
 <script lang="ts">
@@ -49,16 +49,16 @@ import { Component, Watch } from "vue-property-decorator";
 
 @Component({})
 export default class App extends Vue {
-  drawer: boolean = false;
-  isLandingPage: boolean = true;
-  items: { [key: string]: string }[] = [
-    { title: "Dashboard", icon: "mdi-view-dashboard", to: "/home" },
-    { title: "About", icon: "mdi-help-box", to: "/about" }
-  ];
+	drawer: boolean = false;
+	isLandingPage: boolean = true;
+	items: { [key: string]: string }[] = [
+		{ title: "Dashboard", icon: "mdi-view-dashboard", to: "/home" },
+		{ title: "About", icon: "mdi-help-box", to: "/about" }
+	];
 
-  @Watch("$route")
-  checkLandingPage() {
-    this.isLandingPage = this.$route.name === "Landing";
-  }
+	@Watch("$route")
+	checkLandingPage() {
+		this.isLandingPage = this.$route.name === "Landing";
+	}
 }
 </script>
